95eae2014a96321aa7d838c1fb65e91505a40f02
antalovc <antal.ovchinnikov@gmail.com>
2017-10-09 12:05:33 +0300

    3.2 Небольшие исправления

--- js/answer.test.js
+++ js/answer.test.js
@@ -4,15 +4,16 @@ import Answer from './answer.js';
 
 // mocha.setup(`tdd`);
 const assert = chai.assert;
+const strictEqual = assert.strictEqual;
 
 const TESTING_TIME = 100;
 
 describe(`"Answer" class test`, () => {
 
   it(`Testing class constructor`, () => {
-    assert.equal((new Answer(true, 0)).isValid(), true, `valid answer should be created correctly`);
-    assert.equal((new Answer(false, 0)).isValid(), false, `invalid answer should be created correctly`);
-    assert.equal((new Answer(true, TESTING_TIME)).getTime(), TESTING_TIME, `answer time should be set correctly`);
+    strictEqual((new Answer(true, 0)).isValid(), true, `valid answer should be created correctly`);
+    strictEqual((new Answer(false, 0)).isValid(), false, `invalid answer should be created correctly`);
+    strictEqual((new Answer(true, TESTING_TIME)).getTime(), TESTING_TIME, `answer time should be set correctly`);
   });
 
 });
--- js/compute-result.test.js
+++ js/compute-result.test.js
@@ -5,6 +5,7 @@ import {computeResult, RESULT_CONFIGS} from './compute-result.js';
 
 // mocha.setup(`tdd`);
 const assert = chai.assert;
+const strictEqual = assert.strictEqual;
 
 const createAnswersArray = (n, validity, time) => {
   const result = [];
@@ -17,7 +18,7 @@ const createAnswersArray = (n, validity, time) => {
 describe(`"computeResult" function test`, () => {
 
   it(`Testing minimal amount of correct answers`, () => {
-    assert.equal(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN - 1, false, 10), RESULT_CONFIGS.MAX_LIVES), RESULT_CONFIGS.POINTS_WHEN_LOST, `should return correct amount of points when there is too few correct answers`);
+    strictEqual(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN - 1, false, 10), RESULT_CONFIGS.MAX_LIVES), RESULT_CONFIGS.POINTS_WHEN_LOST, `should return correct amount of points when there is too few correct answers`);
     assert(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, 10), RESULT_CONFIGS.MAX_LIVES) > RESULT_CONFIGS.POINTS_WHEN_LOST, `should return correct amount of points when there is just enough correct answers`);
     assert(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN + 1, true, 10), RESULT_CONFIGS.MAX_LIVES) > RESULT_CONFIGS.POINTS_WHEN_LOST, `should return correct amount of points when there are more than enough correct answers`);
   });
@@ -29,12 +30,12 @@ describe(`"computeResult" function test`, () => {
     const nPointsAllNormal = RESULT_CONFIGS.CORRECTS_TO_WIN * RESULT_CONFIGS.POINTS_ANSWER_VALID;
     const nPointsAllSlow = RESULT_CONFIGS.CORRECTS_TO_WIN * (RESULT_CONFIGS.POINTS_ANSWER_VALID + RESULT_CONFIGS.POINTS_ANSWER_SLOW);
 
-    assert.equal(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_FAST - 1), RESULT_CONFIGS.MAX_LIVES), nPointsAllFast + nPointsForMaxLives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} fast answers with ${RESULT_CONFIGS.MAX_LIVES} lives`);
-    assert.equal(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_FAST + 1), RESULT_CONFIGS.MAX_LIVES), nPointsAllNormal + nPointsForMaxLives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} normal answers with ${RESULT_CONFIGS.MAX_LIVES} lives`);
-    assert.equal(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_SLOW + 1), RESULT_CONFIGS.MAX_LIVES), nPointsAllSlow + nPointsForMaxLives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} slow answers with ${RESULT_CONFIGS.MAX_LIVES} lives`);
-    assert.equal(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_FAST - 1), RESULT_CONFIGS.MAX_LIVES - 1), nPointsAllFast + nPointsForMaxMinus1Lives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} fast answers with ${RESULT_CONFIGS.MAX_LIVES - 1} lives`);
-    assert.equal(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_FAST + 1), RESULT_CONFIGS.MAX_LIVES - 1), nPointsAllNormal + nPointsForMaxMinus1Lives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} normal answers with ${RESULT_CONFIGS.MAX_LIVES - 1} lives`);
-    assert.equal(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_SLOW + 1), RESULT_CONFIGS.MAX_LIVES - 1), nPointsAllSlow + nPointsForMaxMinus1Lives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} slow answers with ${RESULT_CONFIGS.MAX_LIVES - 1} lives`);
+    strictEqual(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_FAST - 1), RESULT_CONFIGS.MAX_LIVES), nPointsAllFast + nPointsForMaxLives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} fast answers with ${RESULT_CONFIGS.MAX_LIVES} lives`);
+    strictEqual(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_FAST + 1), RESULT_CONFIGS.MAX_LIVES), nPointsAllNormal + nPointsForMaxLives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} normal answers with ${RESULT_CONFIGS.MAX_LIVES} lives`);
+    strictEqual(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_SLOW + 1), RESULT_CONFIGS.MAX_LIVES), nPointsAllSlow + nPointsForMaxLives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} slow answers with ${RESULT_CONFIGS.MAX_LIVES} lives`);
+    strictEqual(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_FAST - 1), RESULT_CONFIGS.MAX_LIVES - 1), nPointsAllFast + nPointsForMaxMinus1Lives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} fast answers with ${RESULT_CONFIGS.MAX_LIVES - 1} lives`);
+    strictEqual(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_FAST + 1), RESULT_CONFIGS.MAX_LIVES - 1), nPointsAllNormal + nPointsForMaxMinus1Lives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} normal answers with ${RESULT_CONFIGS.MAX_LIVES - 1} lives`);
+    strictEqual(computeResult(createAnswersArray(RESULT_CONFIGS.CORRECTS_TO_WIN, true, RESULT_CONFIGS.TIME_ANSWER_SLOW + 1), RESULT_CONFIGS.MAX_LIVES - 1), nPointsAllSlow + nPointsForMaxMinus1Lives, `should return correct amount of points for ${RESULT_CONFIGS.CORRECTS_TO_WIN} slow answers with ${RESULT_CONFIGS.MAX_LIVES - 1} lives`);
   });
 
 });
